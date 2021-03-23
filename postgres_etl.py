c121207475803492fa6e826d07d6b445c07e1c3e
Kirill <k.demidov@alterosmart.com>
2021-03-23 17:42:28 +0200

    BIG-97

--- postgres_etl.py
+++ postgres_etl.py
@@ -1,27 +1,33 @@
 import psycopg2
 
 def postgres_etl(connection):
-
-    cursor = connection.cursor()
-    cursor.execute("""
-             call public.insert_mng_bridge_user_group() ;
-             call public.insert_mng_user_role_project();
-             call public.insert_stg_bridge_user_group();
-             call public.insert_stg_dim_groups();
-             call public.insert_stg_dim_projects();
-             call public.insert_stg_dim_roles();
-             call public.insert_stg_dim_user_group();
-             call public.insert_stg_dim_user_project_role();
-             call public.insert_stg_dim_users();
-             select public.stg_populate_duration_changelog_table();
-             call public.insert_stg_fact_issue();
-             call public.merge_table ('public','stg_bridge_user_group','public','dwh_bridge_user_group' );
-             call public.merge_table ('public','stg_bridge_user_role_project','public','dwh_bridge_user_role_project' );
-             call public.merge_table ('public','stg_dim_groups','public','dwh_dim_groups' );
-             call public.merge_table ('public','stg_dim_projects','public','dwh_dim_projects' );
-             call public.merge_table ('public','stg_dim_roles','public','dwh_dim_roles' );
-             call public.merge_table ('public','stg_fact_status_duration','public','dwh_fact_status_duration' );
-             call public.merge_table ('public','stg_dim_users','public','dwh_dim_users' );
-             call public.merge_table ('public','stg_fact_issue','public','dwh_fact_issue' );
-             """
-             )
+    try:
+        error = False
+        result = " all finished OK"
+        cursor = connection.cursor()
+        cursor.execute("""
+                 call public.insert_mng_bridge_user_group() ;
+                 call public.insert_mng_user_role_project();
+                 call public.insert_stg_bridge_user_group();
+                 call public.insert_stg_dim_groups();
+                 call public.insert_stg_dim_projects();
+                 call public.insert_stg_dim_roles();
+                 call public.insert_stg_dim_user_group();
+                 call public.insert_stg_dim_user_project_role();
+                 call public.insert_stg_dim_users();
+                 select public.stg_populate_duration_changelog_table();
+                 call public.insert_stg_fact_issue();
+                 call public.merge_table ('public','stg_bridge_user_group','public','dwh_bridge_user_group' );
+                 call public.merge_table ('public','stg_bridge_user_role_project','public','dwh_bridge_user_role_project' );
+                 call public.merge_table ('public','stg_dim_groups','public','dwh_dim_groups' );
+                 call public.merge_table ('public','stg_dim_projects','public','dwh_dim_projects' );
+                 call public.merge_table ('public','stg_dim_roles','public','dwh_dim_roles' );
+                 call public.merge_table ('public','stg_fact_status_duration','public','dwh_fact_status_duration' );
+                 call public.merge_table ('public','stg_dim_users','public','dwh_dim_users' );
+                 call public.merge_table ('public','stg_fact_issue','public','dwh_fact_issue' );
+                 """
+                 )
+    except Exception as e:
+        result = "error " + f"{e}"
+        error = True
+    return error, result
