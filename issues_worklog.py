a5394de700e799cfd9a39370e4e10b139b288e86
Kirill <k.demidov@alterosmart.com>
2021-04-01 17:57:51 +0300

    BIG-111 уточнение записи активного спринта в mrr_worklog

--- issues_worklog.py
+++ issues_worklog.py
@@ -9,15 +9,10 @@ def get_data(data, caption, default=None):
         return default
 
 
-def worklog(auth,connection):
+def worklog(auth, connection):
     error = False
     result = "all finished OK"
-    issue_url = 'https://alterosmart.atlassian.net/browse/'
     startAt = 0
-    total = None
-    sprint_id = -1
-    sprint_name = 'none'
-
     try:
         cursor = connection.cursor()
         url = "https://alterosmart.atlassian.net/rest/api/3/search?jql="
@@ -25,10 +20,10 @@ def worklog(auth,connection):
             "Accept": "application/json"
         }
 
-        needfinish = False
+        need_finish = False
         truncate_table = 'truncate table public.mrr_worklog;'
         cursor.execute(truncate_table)
-        while not needfinish:
+        while not need_finish:
 
             response = requests.request(
                 "GET",
@@ -39,13 +34,11 @@ def worklog(auth,connection):
             issues = json.loads(response.text)
             total = issues['total']
             maxResults = issues['maxResults']
-            needfinish = startAt + maxResults > total
-            #             print (total,startAt,maxResults)
+            need_finish = startAt + maxResults > total
             startAt = startAt + issues['maxResults']
-
-
-            #             print (issues['issues'])
             for issue in issues['issues']:
+                sprint_id = -1
+                sprint_name = ''
                 issue_id = issue['id']
                 if 'fields' in issue:
                     field = issue['fields']
@@ -53,48 +46,40 @@ def worklog(auth,connection):
                         sprint = field['customfield_10020']
                         if sprint and (sprint != 'null'):
                             for sp in sprint:
-                                if sp["state"]=='active':
+                                if sp["state"] == 'active':
                                     sprint_id = sp['id']
                                     sprint_name = sp['name']
-
-
-                                    # print(sprint_id, sprint_name, issue_id)
-
-
-                url_worklog = "https://alterosmart.atlassian.net/rest/api/3/issue/" + issue_id + "/worklog"
-                headers = {
-                    "Accept": "application/json"
-                }
-
-                response = requests.request(
-                    "GET",
-                    url_worklog,
-                    headers=headers,
-                    auth=auth
-                )
-                tmp = ''
-                worklogs = json.loads(response.text)
-                logs = worklogs['worklogs']
-                for log in logs:
-                    if len(str(log)) < 3:
-                       tmp='?'
-                    else:
-                        updater_id = log ['updateAuthor']['accountId']
-                        worklog_id = log['id']
-                        create_date = log['created']
-                        update_date = log['updated']
-                        start_date = log['started']
-                        time_spent = log['timeSpent']
-                        time_spent_sec = log['timeSpentSeconds']
-                         # print(issue_id,updater_id,worklog_id,create_date,update_date,start_date, time_spent,time_spent_sec)
-                        insert_worklog = "INSERT INTO public.mrr_worklog (worklog_id, issue_id, \
-                        create_date, update_date, start_date, time_spent, time_spent_sec, updater_id, active_sprint_id,active_sprint_name)\
-                                        values(" + "'" + str(worklog_id) + "','" + str(issue_id) + "','" + str(create_date) + "','" + \
-                                         str(update_date) + "','" + str(start_date) + "','" + str(time_spent) + "'\
-                                           ,'" + str(time_spent_sec) + "','" + str(updater_id) + "','"+str(sprint_id)+ "','"+str(sprint_name) + "')"
-                        cursor.execute(insert_worklog)
+                                    break
+                            url_worklog = "https://alterosmart.atlassian.net/rest/api/3/issue/" + issue_id + "/worklog"
+                            headers = {"Accept": "application/json"}
+                            response = requests.request(
+                                "GET",
+                                url_worklog,
+                                headers=headers,
+                                auth=auth
+                            )
+                            work_logs = json.loads(response.text)
+                            logs = work_logs['worklogs']
+                            for log in logs:
+                                if len(str(log)) < 3:
+                                    continue
+                                else:
+                                    updater_id = log['updateAuthor']['accountId']
+                                    worklog_id = log['id']
+                                    create_date = log['created']
+                                    update_date = log['updated']
+                                    start_date = log['started']
+                                    time_spent = log['timeSpent']
+                                    time_spent_sec = log['timeSpentSeconds']
+                                    insert_worklog = "INSERT INTO public.mrr_worklog (worklog_id, issue_id, \
+                                    create_date, update_date, start_date, time_spent, time_spent_sec, updater_id,\
+                                    active_sprint_id,active_sprint_name)\
+                                    values(" + "'" + str(worklog_id) + "','" + str(issue_id) + "','" + str(create_date) + "','" + \
+                                                     str(update_date) + "','" + str(start_date) + "','" + str(time_spent) + "'\
+                                                       ,'" + str(time_spent_sec) + "','" + str(updater_id) + "','"+str(sprint_id) + "',\
+                                                       '"+str(sprint_name) + "')"
+                                    cursor.execute(insert_worklog)
     except Exception as e:
         result = "error " + f"{e}"
         error = True
     return error, result
-
